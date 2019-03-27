ef758726543119e72ca445d2993f95a6f23cb458
claimapp <claimapp@xlifang.com>
2019-03-27 11:45:20 +0800

    Toast 添加 Qrcode 增加

--- app/src/main/java/kotlindemo/activity/QrCodeActivity.kt
+++ app/src/main/java/kotlindemo/activity/QrCodeActivity.kt
@@ -1,7 +1,107 @@
 package kotlindemo.activity
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.opengl.Visibility
+import android.os.Bundle
+import android.os.Vibrator
+import android.view.View
+import android.view.View.GONE
+import android.webkit.RenderProcessGoneDetail
+import cn.bingoogolapple.qrcode.core.BarcodeType
+import cn.bingoogolapple.qrcode.core.QRCodeView
+import com.xibei.xdwgkotlin.R
+import kotlindemo.util.Constant.Companion.BASEURL
+import kotlindemo.util.ToastUtil
+import kotlinx.android.synthetic.main.activity_qrcode.*
+import kotlinx.android.synthetic.main.item_toolbar.*
 
-class QrCodeActivity: Activity(){
+class QrCodeActivity: Activity(), View.OnClickListener, QRCodeView.Delegate {
+
+    var flash = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode)
+        iv_back.setOnClickListener(this)
+        iv_flash.setOnClickListener(this)
+        iv_scan.visibility = GONE
+        iv_home.visibility = GONE
+        iv_refresh.visibility = GONE
+        zxingview.setType(BarcodeType.ONLY_QR_CODE,null)
+        zxingview.setDelegate(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.iv_back -> finish()
+            R.id.iv_flash ->{
+                if (flash){
+                    flash = false
+                    iv_flash.setBackgroundResource(R.mipmap.open)
+                    zxingview.closeFlashlight()
+                }else{
+                    flash = true
+                    iv_flash.setBackgroundResource(R.mipmap.close)
+                    zxingview.openFlashlight()
+                }
+            }
+
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        zxingview.startCamera()
+        zxingview.startSpotAndShowRect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        zxingview.stopCamera()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        zxingview.onDestroy()
+    }
+
+
+    private fun vibrate(){
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrator.vibrate(200)
+    }
+
+
+    override fun onScanQRCodeSuccess(result: String?) {
+        vibrate()
+        if (result!!.length>=18){
+            if (result.substring(0,18) ==BASEURL){
+                val data = Intent()
+                data.putExtra("result",result)
+                setResult(1001,data)
+                finish()
+            }else{
+                ToastUtil.ToastUtil.showText("请扫描正确二维码！")
+                zxingview.startSpotAndShowRect()
+                return
+            }
+        }
+
+    }
+
+    override fun onCameraAmbientBrightnessChanged(isDark: Boolean) {
+        if (isDark&&!flash){
+            ToastUtil.ToastUtil.showText("光线太暗，青请到光亮的地方或者打开闪光灯")
+        }
+    }
+
+    override fun onScanQRCodeOpenCameraError() {
+        ToastUtil.ToastUtil.showText("打开摄像头出错")
+    }
+
+
 
 }
--- app/src/main/java/kotlindemo/util/ToastUtil.kt
+++ app/src/main/java/kotlindemo/util/ToastUtil.kt
@@ -3,22 +3,64 @@ package kotlindemo.util
 import android.app.Activity
 import android.content.Context
 import android.widget.Toast
+import kotlindemo.MyApplication
 
 class ToastUtil{
-     var mToast: Toast? = null
-     var mContext:Context ?= null
-     var toast : Toast? = null
 
-     fun ToastUtil(){
+    object ToastUtil{
+        private var mToast: Toast? = null
 
-     }
+        var mContext = MyApplication.instance
 
-    fun toUi(context: Activity,content:String){
-        context.runOnUiThread(Runnable {
-            kotlin.run {
+        private val toast: Toast? = null
+
+        fun showText(text: CharSequence) {
+            if (mContext != null) {
+                if (mToast == null) {
+                    mToast = Toast.makeText(mContext, text, Toast.LENGTH_SHORT)
+                } else {
+                    mToast!!.setText(text)
+                    mToast!!.duration = Toast.LENGTH_SHORT
+                }
+                try {
+                    mToast!!.show()
+                } catch (var2: Throwable) {
+                    var2.printStackTrace()
+                }
 
             }
-        })
+        }
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    companion object {
+//
+//        private lateinit var mToast:Toast
+//        private val mContext = MyApplication.javaClass.newInstance()
+//
+//        fun showText(text: CharSequence){
+//            mToast = Toast.makeText(mContext,text,Toast.LENGTH_SHORT)
+//        }
+//    }
 }
